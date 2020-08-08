f8c2b166fd6863bfccfe438c2f1e2565655b2b9e
Rodrigo Seguel <63627567+Khalzz@users.noreply.github.com>
2020-08-08 12:48:45 -0400

    Update ServoPorStick2.ino

--- ServoPorStick2.ino
+++ ServoPorStick2.ino
@@ -15,24 +15,32 @@ void setup()
 void loop()
 {
   
+  //antes de continuar, recuerden que los valores de stick van desde 0 hasta 1023
+  //el servo tiene como valor minimo 0 y maximo 180 (por lo menos el que uso yo para este proyecto
+  
   joystick = analogRead(stickX);
   
+  //estado neutral de stick
   if (joystick > 400  && joystick < 600){
     servoEstado = servoEstado;
   }
   
+  //estado de stick (cuando este aumenta)
   if(joystick <= 450){
      servoEstado = servoEstado + 3;
   }
   
+  //estado de stick (cuanddo este disminuye)
   else if(joystick >= 550){
      servoEstado = servoEstado - 3;
   }
   
+  //estad0 maximo de servo
   if (servoEstado >= 180){
     servoEstado = 180;
   }
   
+  //estado minimo de servo
   else if (servoEstado <= 0){
     servoEstado = 0;
   }
